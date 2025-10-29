5a4a92ed900fa46809d9a10d2c3b6c5a345cf4c4
kote5615 <miss.vestik@yandex.ru>
2025-10-29 17:34:27 +0500

    подключила spring к проекту

--- backend/src/main/java/com/MWS/server/CloudStorageServer.java
+++ backend/src/main/java/com/MWS/server/CloudStorageServer.java
@@ -1,7 +1,14 @@
 package com.MWS.server;
 
-public class cloudStorageServer {
-    static void main() {
+//import com.MWS.handlers.CloudStorageServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CloudStorageServer {
+
+    public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println(String.format("Hello and welcome!"));
@@ -10,6 +17,8 @@ public class cloudStorageServer {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
         }
+        SpringApplication.run(CloudStorageServer.class, args);
     }
 }
