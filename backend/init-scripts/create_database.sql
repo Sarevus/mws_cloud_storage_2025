e0d7869145b33aab28b33cb7e0016482edbf4a76
kote5615 <miss.vestik@yandex.ru>
2025-11-15 18:41:29 +0500

    исправила несоответствие типов данных id

--- backend/init-scripts/create_database.sql
+++ backend/init-scripts/create_database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     phone_number VARCHAR(50) NOT NULL
@@ -7,7 +7,7 @@ CREATE TABLE users (
 
 CREATE TABLE files (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    user_id UUID NOT NULL,
     link VARCHAR NOT NULL,
 
     CONSTRAINT fk_files_users_id
