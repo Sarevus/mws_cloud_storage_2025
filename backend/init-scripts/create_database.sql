a2d5a80cc282d182e348260687603b3cec59aa46
bodyaginaelizaveta <embodyagina@gmail.com>
2025-11-16 15:49:55 +0300

    Третья таблица с метаданными

--- backend/init-scripts/create_database.sql
+++ backend/init-scripts/create_database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     phoneNumber VARCHAR(50) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE files (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
     link VARCHAR NOT NULL,
 
@@ -15,4 +15,13 @@ CREATE TABLE files (
         FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE metadata (
+    file_id UUID PRIMARY KEY REFERENCES files(id) ON DELETE CASCADE,
+    filename VARCHAR(255) NOT NULL,
+    file_size BIGINT NOT NULL,
+    description TEXT,
+    tags VARCHAR(500),
+    is_public BOOLEAN DEFAULT FALSE
 );
