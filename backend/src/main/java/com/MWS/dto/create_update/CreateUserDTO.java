bfd952ca933dd21ac6533c2b43e06d00218b024e
kote5615 <miss.vestik@yandex.ru>
2025-11-16 13:53:42 +0500

    добавлены аннотации для CreateUserDTOdto

--- backend/src/main/java/com/MWS/dto/create_update/CreateUserDTO.java
+++ backend/src/main/java/com/MWS/dto/create_update/CreateUserDTO.java
@@ -1,9 +1,21 @@
 package com.MWS.dto.create_update;
 
+import com.MWS.Validator.annotations.*;
+
 public record CreateUserDTO(
+        @NotNull(message = "Имя не может быть пустым")
+        @Size(min = 2, max = 50, message = "Имя должно быть от 2 до 50 символов")
         String name,
+
+        @NotNull(message = "Email не может быть пустым")
+        @Email(message = "Некорректный формат email")
         String email,
+
+        @NotNull(message = "Телефон обязателен")
+        @PhoneNumber(message = "Некорректный номер телефона")
         String phoneNumber,
+
+        @NotNull(message = "Пароль обязателен")
+        @Size(min = 6, max = 100, message = "Пароль должен быть от 6 до 100 символов")
         String password
-) {
-}
+) {}
