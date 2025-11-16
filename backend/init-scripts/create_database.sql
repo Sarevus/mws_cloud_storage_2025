0835d8bf66392ab11c5dabc47af8f108bc120035
bodyaginaelizaveta <embodyagina@gmail.com>
2025-11-16 13:55:47 +0300

    not null в БД

--- backend/init-scripts/create_database.sql
+++ backend/init-scripts/create_database.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    phoneNumber VARCHAR(50),
+    phoneNumber VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL
 );
 
