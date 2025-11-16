1f59807f3b0e199b995ec7ec5e0d5756ebb211f1
kote5615 <miss.vestik@yandex.ru>
2025-11-16 13:53:15 +0500

    добавлена проверка валидацией

--- backend/src/main/java/com/MWS/service/UserServiceRelease.java
+++ backend/src/main/java/com/MWS/service/UserServiceRelease.java
@@ -1,5 +1,7 @@
 package com.MWS.service;
 
+import com.MWS.Validator.ValidationResult;
+import com.MWS.Validator.Validator;
 import com.MWS.dto.create_update.CreateUserDTO;
 import com.MWS.dto.get.GetSimpleUserDto;
 import com.MWS.handlers.UserController;
@@ -8,6 +10,7 @@ import com.MWS.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.MWS.security.HashPassword;
 
 import java.util.UUID;
 
@@ -20,12 +23,27 @@ public class UserServiceRelease implements UserService {
     }
 
     @Override
-    public GetSimpleUserDto createUser(CreateUserDTO userDTO) {
-        // Проверяем, существует ли пользователь с таким email
+    public GetSimpleUserDto createUser(CreateUserDTO userDTO){
+
+        ValidationResult validationResult = Validator.validate(userDTO);
+        if (!validationResult.isValid() || !validationResult.getErrors().isEmpty()) {
+            String message = String.join("; ", validationResult.getErrors());
+            logger.warn("Ошибка валидации данных пользователя: {}", message);
+            throw new IllegalArgumentException("Некорректные данные пользователя: " + message);
+        }
+
+        /**
+         * Заполняем переменные значениями из dto. Пароль переводим в хэшированный
+         */
+
         String email = userDTO.email();
         String name = userDTO.name();
         String phoneNumber = userDTO.phoneNumber();
-        String password = userDTO.password();
+        String password = HashPassword.createPasswordHash(userDTO.password());
+
+        /**
+         * Проверяем есть ли в бд пользователь с таким же email
+         */
         userRepository.findByEmail(email).ifPresent(existingUser -> {
             logger.warn("Пользователь с email {} уже существует.", email);
             throw new IllegalArgumentException("Email " + email + " уже занят.");
@@ -35,6 +53,8 @@ public class UserServiceRelease implements UserService {
         user.setEmail(email);
         user.setPhoneNumber(phoneNumber);
         user.setPassword(password);
+
+
         UserEntity savedUser = userRepository.save(user);
 
         return new GetSimpleUserDto(
