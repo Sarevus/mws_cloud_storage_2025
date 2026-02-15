4fd733f31b6887241a34be6de3677961a7a1c224
bodyaginaelizaveta <embodyagina@gmail.com>
2026-02-15 13:30:35 +0300

    Перевела на Spring Home; ControllerResult больше не нужен

--- backend/src/main/java/com/MWS/handlers/Home.java
+++ backend/src/main/java/com/MWS/handlers/Home.java
@@ -1,7 +1,14 @@
 package com.MWS.handlers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // методы контроллера возвращают JSON
 public class Home {
-    public static Object check(spark.Request request, spark.Response response) {
+
+    // метод вызывается на гет-запрос
+    @GetMapping("/")
+    public String home() {
         return "Home Page";
     }
 }
