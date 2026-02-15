2297d3bcb5dc31e04746506b01d0b37075beba0c
bodyaginaelizaveta <embodyagina@gmail.com>
2026-02-15 15:02:03 +0300

    Точка входа в Spring Boot приложение

--- backend/src/main/java/com/MWS/CloudStorageApplication.java
+++ backend/src/main/java/com/MWS/CloudStorageApplication.java
@@ -1,11 +1,46 @@
 package com.MWS;
-
+import com.MWS.storage.Database;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CloudStorageApplication {
+    private static final Logger logger = LoggerFactory.getLogger(com.MWS.CloudStorageApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CloudStorageApplication.class, args);
+    }
+
+    /**
+     * Проверка подключения к БД
+     */
+    @PostConstruct
+    public void checkDatabase() {
+        if (!Database.testConnection()) {
+            logger.error("Не удалось подключиться к базе данных!");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Включает CORS для всех API запросов
+     * фронт сможет обратиться к бэку
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
