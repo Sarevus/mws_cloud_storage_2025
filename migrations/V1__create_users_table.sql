20c26fa960903d5ab28620916e97e1f68e34beb7
bodyaginaelizaveta <embodyagina@gmail.com>
2026-02-16 19:02:05 +0300

    Тут были проблемы с именем колонки. Там Hibernate создавал таблицу и переименовывал колонку

--- migrations/V1__create_users_table.sql
+++ migrations/V1__create_users_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    phoneNumber VARCHAR(50) NOT NULL,
+    phonenumber VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL
 );
