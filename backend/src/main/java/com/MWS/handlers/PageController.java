ab9b6b9bbf33f7b94dfb15a842c9c13928b935ff
bodyaginaelizaveta <embodyagina@gmail.com>
2026-02-16 19:06:12 +0300

    Исправила пути к страницам

---
+++ backend/src/main/java/com/MWS/handlers/PageController.java
@@ -0,0 +1,38 @@
+package com.MWS.handlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "forward:/registerIndex.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "forward:/loginIndex.html";
+    }
+
+    @GetMapping("/files")
+    public String files() {
+        return "forward:/fileExchange.html";
+    }
+
+    @GetMapping("/myProfile")
+    public String myProfile() {
+        return "forward:/myProfile.html";
+    }
+
+    @GetMapping("/editProfile")
+    public String editProfile() {
+        return "forward:/editProfile.html";
+    }
+}
