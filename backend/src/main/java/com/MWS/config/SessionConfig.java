703dad297c1604f0505e3684a1bb88c285e1a69f
bodyaginaelizaveta <embodyagina@gmail.com>
2026-02-22 10:40:55 +0300

    Настройки Redis для хранения сессий

---
+++ backend/src/main/java/com/MWS/config/SessionConfig.java
@@ -0,0 +1,34 @@
+package com.MWS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 604800)   // включает поддержку сессий через Redit
+public class SessionConfig {
+
+    @Bean
+    // RedisConnectionFactory - интерфейс для создания подключения к Redis
+    // LettuceConnectionFactory - конкретная реализация
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                Config.getRedisHost(),
+                Config.getRedisPort()
+        );
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSION");
+        serializer.setCookiePath("/");
+        serializer.setCookieMaxAge(604800); // 7 дней
+        serializer.setUseHttpOnlyCookie(true);
+        return serializer;
+    }
+}
