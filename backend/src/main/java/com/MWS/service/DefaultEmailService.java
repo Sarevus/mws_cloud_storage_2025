57b59c61f23faf8ce98799039ff4cecf0907310f
kote5615 <miss.vestik@yandex.ru>
2026-03-23 17:21:10 +0300

    добавлена функция в сервис для отправки html в email

--- backend/src/main/java/com/MWS/service/DefaultEmailService.java
+++ backend/src/main/java/com/MWS/service/DefaultEmailService.java
@@ -9,14 +9,22 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.io.FileNotFoundException;
 
 @Service
 public class DefaultEmailService implements EmailService {
 
-    @Autowired
     public JavaMailSender emailSender;
+    private final TemplateEngine templateEngine;
+
+    @Autowired
+    public DefaultEmailService(JavaMailSender emailSender, TemplateEngine templateEngine) {
+        this.emailSender = emailSender;
+        this.templateEngine = templateEngine;
+    }
 
     @Override
     public void sendTextEmail(String toAddress, String subject, String message) {
@@ -39,6 +47,20 @@ public class DefaultEmailService implements EmailService {
         messageHelper.setText(message);
         FileSystemResource file = new FileSystemResource(ResourceUtils.getFile(attachment));
         messageHelper.addAttachment("file-name:", file);
+        emailSender.send(mimeMessage);
+    }
+
+    public void sendHtmlEmail(String to, String subject, String template, Context context) throws MessagingException, FileNotFoundException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper htmlMessageSender = new MimeMessageHelper(mimeMessage, true);
+
+        String htmlContent = templateEngine.process(template, context);
+
+        htmlMessageSender.setTo(to);
+        htmlMessageSender.setFrom("CloudStorageMIPT@yandex.com");
+        htmlMessageSender.setSubject(subject);
+        htmlMessageSender.setText(htmlContent, true); // Set true for HTML content
+
         emailSender.send(mimeMessage);
     }
 }
